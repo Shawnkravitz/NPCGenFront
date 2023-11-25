43b4d4c59bcdc2ad41d76b10f117007ad25d8432
Shawn K <shawnkravitz@gmail.com>
2023-11-25 00:26:24 -0500

    Updated CSS to add background image and format text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
- 
-function App({ name = "World" }) {
-  const [data, setData] = useState({ id: null, content: '' });
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+function App() {
+  const [description, setDescription] = useState({
+    id: null,
+    greeting: '',
+  });
 
-  useEffect(() => {
-      axios.get(`http://localhost:8081/greeting`, { params: { name } })
-          .then(response => {
-              setData(response.data);
-              setIsLoading(false);
-          })
-          .catch(error => {
-              console.error('There was a problem with the Axios operation:', error);
-              setError(error);
-              setIsLoading(false);
-          });
-  }, [name]); // Dependency array with 'name' means this effect will rerun when 'name' changes
+  // Function to update the description
+  const fetchDescription = (name = "World") => {
+    axios.get(`http://localhost:8081/greeting`, { params: { name } })
+      .then(response => {
+        // Update the description in the state
+        setDescription({
+          id: response.data.id,
+          greeting: response.data.content,
+        });
+      })
+      .catch(error => {
+        console.error('There was a problem with the Axios operation:', error);
+      });
+  };
 
-  if (isLoading) {
-      return <div>Loading...</div>;
-  }
-
-  if (error) {
-      return <div>Error: {error.message}</div>;
-  }
+  // This function will be called when "Generate Character" button is clicked
+  const handleGenerateCharacter = () => {
+    // Here you can call fetchDescription with a specific name or just leave it to default to "World"
+    fetchDescription(); // Or pass a specific character name
+  };
 
   return (
-      <div>
-          <p>ID: {data.id}</p>
-          <p>Greeting: {data.content}</p>
+    <div className="app">
+      <button onClick={handleGenerateCharacter}>Generate Character</button>
+      <div className="npc-display">
+        <div className="description">
+          <h2>Character Name:</h2> <p>{description.greeting}</p>
+          <p><strong>ID:</strong> {description.id}</p>
+          {/* Adding the Generate Character button */}
+          
+        </div>
+        <div className="description">
+          <h2>Description</h2>
+          <p><strong>ID:</strong> {description.id}</p>
+          <p><strong>Greeting:</strong> {description.greeting}</p>
+          {/* Adding the Generate Character button */}
+          
+        </div>
+        <div className="description">
+          <h2>Personality</h2>
+          <p><strong>ID:</strong> {description.id}</p>
+          <p><strong>Greeting:</strong> {description.greeting}</p>
+          {/* Adding the Generate Character button */}
+          
+        </div>
+        {/* ...additional sections like Personality, Abilities, etc. */}
       </div>
+    </div>
   );
 }
 
